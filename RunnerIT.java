535f1a4b086b61e0d4e6b4e038a8ddc2faf0a8fc
Nelson <33600855+GitHubNels@users.noreply.github.com>
2024-11-03 13:04:27 +0530

    Create RunnerIT.java

---
+++ RunnerIT.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/main/java/com/features", glue = "com.stepDefinitions")
+public class RunnerIT {
+}
